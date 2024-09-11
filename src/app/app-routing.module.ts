bd253742470e528f80cafe076408f42b64ef586f
MihajloZivkovic1 <85947846+MihajloZivkovic1@users.noreply.github.com>
2024-09-11 09:34:07 +0200

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from 'src/components/details/details.component';
+import { HomeComponent } from 'src/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: 'search/:game-search',
+    component: HomeComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+
+
+// import { GaugeModule } from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+// import { MatFormField } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+import { SearchBarComponent } from '../components/search-bar/search-bar.component';
+import { HomeComponent } from 'src/components/home/home.component';
+import { HttpHeadersInterceptor } from './interceptors/https-headers.interceptors';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptors';
+import { DetailsComponent } from 'src/components/details/details.component';
+import { GameTabsComponent } from 'src/components/game-tabs/game-tabs.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent,
+    DetailsComponent,
+    GameTabsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSelectModule,
+    // GaugeModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
